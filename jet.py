237bc72cb118174b8aacc84b3234006aceb73d7c
Ellis Collins <ec765@cam.ac.uk>
2025-01-21 13:48:01 +0000

    OOP edits

--- jet.py
+++ jet.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import matplotlib
 import numpy as np
 from flightcondition import FlightCondition, unit
@@ -6,160 +8,157 @@ from thermo.interaction_parameters import IPDB
 import matplotlib.pyplot as plt
 from svgpathtools import parse_path, svg2paths
 
-# Flight Condition
 
-condition = FlightCondition(M=0.85, L=3.8 * unit('m'), h=37000 * unit('ft'), units='SI')
-print(condition)
+@dataclass
+class Engine:
+    D: float
+    BPR: float
+    sfc_cruise: float
 
-# Chemistry
-constants, properties = ChemicalConstantsPackage.from_IDs(['water', 'carbon dioxide', 'oxygen', 'nitrogen'])
-kijs = IPDB.get_ip_asymmetric_matrix('ChemSep PR', constants.CASs, 'kij')
-eos_kwargs = {'Pcs': constants.Pcs, 'Tcs': constants.Tcs, 'omegas': constants.omegas, 'kijs': kijs}
-gas = CEOSGas(PRMIX, eos_kwargs=eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases)
-liquid = CEOSLiquid(PRMIX, eos_kwargs=eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases)
-flasher = FlashVL(constants, properties, liquid=liquid, gas=gas)
+    eta_c: float
+    eta_f: float
+    eta_t: float
 
-R = 287 * unit('m^2/s^2/K')
-gam_a = 1.4
-cP_a = 1005 * unit('kg.m^2/s^2/kg/K')
-gam_e = 1.31
-cP_e = 830 * unit('kg.m^2/s^2/kg/K')
+    r_pf: float
+    r_po: float
+    Vjb_Vjc: float
 
-eta_c = 0.85
-eta_f = 0.92345
-eta_t = 0.9
-
-r_pf = 1.4398
-r_pc = 42 / r_pf # PW1100G
-r_pt = np.nan
-Vjb_Vjc = 0.8
-
-BPR = 12.5 # PW1100G
-D = (81 * unit('in')).to('m') # PW1100G
-sfc_cruise_des = 0.0144 * unit('kg/kN/s') # PW1100G
-LD = 18 # A320
-L = 55000 * 0.00981 * unit('kN') # A320
-F_cruise_des = L / LD / 2
-mdotf = sfc_cruise_des * F_cruise_des
-LCV_fuel = 43.2 * unit('MJ/kg') # Jet A-1
-C_f = 10.8 # Jet A-1
-H_f = 21.6 # Jet A-1
-M_f = 151.9 # Jet A-1
-M_H2O = 18.02
-M_CO2 = 44.01
-M_N2 = 28.01
-M_O2 = 32.00
-
-# TET can be 1550K
-# EGT can be 15-25% for cooling - taken after HPC
-# epsilon Tg-Tm / Tg-Tc for cooling is around 0.6-0.7
-
-pa = condition.p
-
-h01 = 249195 * unit('J/kg')
-p01 = condition.p0
-T01 = condition.T0
-ro01 = p01 / R / T01
-
-p03b = p02 = p01 * r_pf
-T03b = T02 = T01 * r_pf ** ((gam_a - 1) / (gam_a * eta_f))
-ro03b = ro02 = ro01 * r_pf ** (1 / gam_a / eta_f)
-
-p3b = p03b * (1 + (gam_a - 1) / 2) ** -(gam_a / (gam_a - 1))
-T3b = T03b * (1 + (gam_a - 1) / 2) ** -1
-ro3b = ro03b * (1 + (gam_a - 1) / 2) ** -(1 / (gam_a - 1))
-
-p03 = p02 * r_pc
-T03 = T02 * r_pc ** ((gam_a - 1) / (gam_a * eta_c))
-wc = cP_a * (T03 - T02)
-h03 = h01 + wc
-
-# assume bypass is choked. 1/2Vjb^2 = cP(T03b-T3b) OR Vjb = sqrt(gam_a * R * T3b)
-Vjb = np.sqrt(2 * cP_a * (T03b - T3b))
-Vf = condition.TAS
-
-tb = (Vjb - Vf + (p3b - pa) / (ro3b * Vjb))
-
-wfan = cP_a * (T02 - T01)
-wcore = BPR * wfan
-
-Vjc = Vjb / Vjb_Vjc
-
-Q = mdotf * LCV_fuel
-
-tc = (Vjc - Vf)
-
-mdotc = F_cruise_des / (tc + BPR * tb)
-mdotb = mdotc * BPR
-
-p04 = 0.99 * p03
-h04 = h03 + Q / mdotc
-
-#  C 10.8 H 21.6 + phi*(C_f+H_f/4)(O2 + 79/21N2) --> C_f CO2 + H_f/2 H2O + phi*(C_f+H_f/4) - H_f/4 O2 + phi*(C_f+H_f/4) * 79/21 N2
-AFR_s = (C_f + H_f / 4) * (M_O2 + 79 / 21 * M_N2) / M_f
-AFR = mdotc / mdotf
-phi = (AFR / AFR_s).to_base_units()
-
-nCO2 = C_f
-nH2O = H_f / 2
-nO2 = phi * (C_f + H_f / 4) - nH2O / 2 - nCO2
-nN2 = phi * (C_f + H_f / 4) * 79 / 21
-nTOTAL = nCO2 + nH2O + nO2 + nN2
-MTOTAL = nCO2 * M_H2O + nH2O * M_H2O + nO2 * M_O2 + nN2 * M_H2O
-
-wtCO2 = (nCO2 * M_CO2) / MTOTAL
-wtH2O = (nH2O * M_H2O) / MTOTAL
-wtO2 = (nO2 * M_O2) / MTOTAL
-wtN2 = (nN2 * M_N2) / MTOTAL
-
-zs = [wtH2O, wtCO2, wtO2, wtN2]
-
-T04 = flasher.flash(H_mass=h04.magnitude, P=p04.magnitude, zs=zs).T * unit('K')
-
-h05 = h04 - wc - wcore
-T05 = flasher.flash(H_mass=h05.magnitude, P=p04.magnitude, zs=zs).T * unit('K')
-r_pt = (T04 / T05) ** (gam_e / (gam_e - 1) / eta_t)
-p05 = p04 / r_pt
-ST05 = flasher.flash(H_mass=h05.magnitude, P=p05.magnitude, zs=zs).T * unit('K')
-T5 = T05 - 0.5 / cP_e * Vjb ** 2
-r_pt = (T04 / T05) ** (gam_e / (gam_e - 1) / eta_t)
-p05 = p04 / r_pt
-
-print(T04, T05, T5, T03 ** 2 / T02)
-print(f"{r_pt=}\n")
-
-Mjc = Vjc / np.sqrt(gam_e * R * T5)
-print(Mjc)
-
-Ab = mdotb * np.sqrt(cP_a * T03b) / 1.281 / p03b
-Ac = mdotc * np.sqrt(cP_e * T05) / (
-            gam_e / np.sqrt(gam_e - 1) * Mjc * (1 + (gam_e - 1) / 2 * Mjc ** 2) ** 0.5) / condition.p
-
-r = np.linspace(0.00000001*unit('m'), D / 2)
-b_annulus_t = Ab / 2 / np.pi / r
-c_annulus_t = Ac / 2 / np.pi / r
-
-# r = [0.4,0.74]
-
-print(f"BYPASS:\n"
-      f"p03b = {p03b.to('kPa'):.3f}\n"
-      f"h03b = {(cP_a * (T02 - T01) + h01).to('kJ/kg')}\n"
-      f"Ab = {Ab.to_base_units():.3f}\n"
-      f"\nCORE:\n"
-      f"wH2O = {wtH2O:.5f}\n"
-      f"p05 = {p05.to('kPa'):.3f}\n"
-      f"h05 = {h05.to('kJ/kg')}\n"
-      f"Ac = {Ac.to_base_units():.3f}\n")
-
-print((mdotc*p05+mdotb*p03b)/(mdotc+mdotb))
-print((mdotc*h05+mdotb*(cP_a * (T02 - T01) + h01))/(mdotc+mdotb))
+    @property
+    def r_pc(self):
+        return self.r_po / self.r_pf
 
 
-# plt.plot(r, np.clip((r + b_annulus_t / 2).magnitude,0,(D/2).magnitude), color='blue', label='BYPASS')
-# plt.plot(r, np.clip((r - b_annulus_t / 2).magnitude, 0, (D/2).magnitude), color='blue')
-# plt.plot(r, np.clip((r + c_annulus_t / 2).magnitude,0,(D/2).magnitude), color='red', label='CORE')
-# plt.plot(r, np.clip((r - c_annulus_t / 2).magnitude, 0, (D/2).magnitude), color='red')
-# plt.legend()
-# plt.xlabel('$r$ [m]')
-# plt.ylabel('annulus extent [m]')
-# plt.show()
+class JetCondition:
+    def __init__(self, flight_condition, engine):
+        self.fc = flight_condition
+        self.engine = engine
+
+        # chemistry
+        constants, properties = ChemicalConstantsPackage.from_IDs(['water', 'carbon dioxide', 'oxygen', 'nitrogen'])
+        kijs = IPDB.get_ip_asymmetric_matrix('ChemSep PR', constants.CASs, 'kij')
+        eos_kwargs = {'Pcs': constants.Pcs, 'Tcs': constants.Tcs, 'omegas': constants.omegas, 'kijs': kijs}
+        gas = CEOSGas(PRMIX, eos_kwargs=eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases)
+        liquid = CEOSLiquid(PRMIX, eos_kwargs=eos_kwargs, HeatCapacityGases=properties.HeatCapacityGases)
+        self.flasher = FlashVL(constants, properties, liquid=liquid, gas=gas)
+
+        # gas properties
+        R = 287 * unit('m^2/s^2/K')
+        gam_a = 1.4
+        cP_a = 1005 * unit('kg.m^2/s^2/kg/K')
+        gam_e = 1.31
+        cP_e = 830 * unit('kg.m^2/s^2/kg/K')
+
+        LD = 18  # A320
+        L = 60000 * 0.00981 * unit('kN')  # A320
+        F_cruise_des = L / LD / 2
+        mdotf = self.engine.sfc_cruise * F_cruise_des
+        LCV_fuel = 43.2 * unit('MJ/kg')  # Jet A-1
+        C_f = 10.8  # Jet A-1
+        H_f = 21.6  # Jet A-1
+        M_f = 151.9  # Jet A-1
+        M_H2O = 18.02
+        M_CO2 = 44.01
+        M_N2 = 28.01
+        M_O2 = 32.00
+
+        # TET can be 1550K
+        # EGT can be 15-25% for cooling - taken after HPC
+        # epsilon Tg-Tm / Tg-Tc for cooling is around 0.6-0.7
+
+        pa = self.fc.p
+
+        h01 = 249195 * unit('J/kg')
+        p01 = self.fc.p0
+        T01 = self.fc.T0
+        ro01 = p01 / R / T01
+
+        p03b = p02 = p01 * self.engine.r_pf
+        T03b = T02 = T01 * self.engine.r_pf ** ((gam_a - 1) / (gam_a * self.engine.eta_f))
+        ro03b = ro02 = ro01 * self.engine.r_pf ** (1 / gam_a / self.engine.eta_f)
+
+        p3b = p03b * (1 + (gam_a - 1) / 2) ** -(gam_a / (gam_a - 1))
+        T3b = T03b * (1 + (gam_a - 1) / 2) ** -1
+        ro3b = ro03b * (1 + (gam_a - 1) / 2) ** -(1 / (gam_a - 1))
+
+        p03 = p02 * self.engine.r_pc
+        T03 = T02 * self.engine.r_pc ** ((gam_a - 1) / (gam_a * self.engine.eta_c))
+        wc = cP_a * (T03 - T02)
+        h03 = h01 + wc
+
+        # assume bypass is choked. 1/2Vjb^2 = cP(T03b-T3b) OR Vjb = sqrt(gam_a * R * T3b)
+        Vjb = np.sqrt(2 * cP_a * (T03b - T3b))
+        Vf = self.fc.TAS
+
+        tb = (Vjb - Vf + (p3b - pa) / (ro3b * Vjb))
+
+        wfan = cP_a * (T02 - T01)
+        wcore = self.engine.BPR * wfan
+
+        Vjc = Vjb / engine.Vjb_Vjc
+
+        Q = mdotf * LCV_fuel
+
+        tc = (Vjc - Vf)
+
+        mdotc = F_cruise_des / (tc + self.engine.BPR * tb)
+        mdotb = mdotc * self.engine.BPR
+
+        p04 = 0.99 * p03
+        h04 = h03 + Q / mdotc
+
+        #  C 10.8 H 21.6 + phi*(C_f+H_f/4)(O2 + 79/21N2) --> C_f CO2 + H_f/2 H2O + phi*(C_f+H_f/4) - H_f/4 O2 + phi*(C_f+H_f/4) * 79/21 N2
+        AFR_s = (C_f + H_f / 4) * (M_O2 + 79 / 21 * M_N2) / M_f
+        AFR = mdotc / mdotf
+        phi = (AFR / AFR_s).to_base_units()
+
+        nCO2 = C_f
+        nH2O = H_f / 2
+        nO2 = phi * (C_f + H_f / 4) - nH2O / 2 - nCO2
+        nN2 = phi * (C_f + H_f / 4) * 79 / 21
+        nTOTAL = nCO2 + nH2O + nO2 + nN2
+        MTOTAL = nCO2 * M_H2O + nH2O * M_H2O + nO2 * M_O2 + nN2 * M_H2O
+
+        wtCO2 = (nCO2 * M_CO2) / MTOTAL
+        wtH2O = (nH2O * M_H2O) / MTOTAL
+        wtO2 = (nO2 * M_O2) / MTOTAL
+        wtN2 = (nN2 * M_N2) / MTOTAL
+
+        zs = [wtH2O, wtCO2, wtO2, wtN2]
+
+        T04 = self.flasher.flash(H_mass=h04.magnitude, P=p04.magnitude, zs=zs).T * unit('K')
+
+        h05 = h04 - wc - wcore
+        T05 = self.flasher.flash(H_mass=h05.magnitude, P=p04.magnitude, zs=zs).T * unit('K')
+        r_pt = (T04 / T05) ** (gam_e / (gam_e - 1) / engine.eta_t)
+        p05 = p04 / r_pt
+        T05 = self.flasher.flash(H_mass=h05.magnitude, P=p05.magnitude, zs=zs).T * unit('K')
+        T5 = T05 - 0.5 / cP_e * Vjb ** 2
+        r_pt = (T04 / T05) ** (gam_e / (gam_e - 1) / engine.eta_t)
+        p05 = p04 / r_pt
+
+        print(T04, T05, T5, T03 ** 2 / T02)
+        print(f"{r_pt=}\n")
+
+        Mjc = Vjc / np.sqrt(gam_e * R * T5)
+        print(Mjc)
+
+        self.Ab = mdotb * np.sqrt(cP_a * T03b) / 1.281 / p03b
+        self.Ac = mdotc * np.sqrt(cP_e * T05) / (
+                gam_e / np.sqrt(gam_e - 1) * Mjc * (1 + (gam_e - 1) / 2 * Mjc ** 2) ** 0.5) / self.fc.p
+
+        r = np.linspace(0.00000001 * unit('m'), engine.D / 2)
+        b_annulus_t = self.Ab / 2 / np.pi / r
+        c_annulus_t = self.Ac / 2 / np.pi / r
+
+        print(f"BYPASS:\n"
+              f"p03b = {p03b.to('kPa'):.3f}\n"
+              f"h03b = {(cP_a * (T02 - T01) + h01).to('kJ/kg')}\n"
+              f"Ab = {self.Ab.to_base_units():.3f}\n"
+              f"\nCORE:\n"
+              f"wH2O = {wtH2O:.5f}\n"
+              f"p05 = {p05.to('kPa'):.3f}\n"
+              f"h05 = {h05.to('kJ/kg')}\n"
+              f"Ac = {self.Ac.to_base_units():.3f}\n")
+
+        print((mdotc * p05 + mdotb * p03b) / (mdotc + mdotb))
+        print((mdotc * h05 + mdotb * (cP_a * (T02 - T01) + h01)) / (mdotc + mdotb))
--- nacelle.py
+++ nacelle.py
@@ -1,5 +1,5 @@
 import numpy as np
-from flightcondition import unit
+from flightcondition import FlightCondition, unit
 from matplotlib import pyplot as plt
 from matplotlib.colors import TABLEAU_COLORS
 from svgpathtools import svg2paths
@@ -7,6 +7,7 @@ from ansys.geometry.core import __version__, launch_modeler
 from ansys.geometry.core.sketch import Sketch
 from ansys.geometry.core.math import Point2D, Vector3D, Point3D, UNITVECTOR3D_X, UNITVECTOR3D_Y, ZERO_POINT3D
 from pathlib import Path
+from jet import JetCondition, Engine
 
 print(f"PyAnsys Geometry version: {__version__}")
 
@@ -17,7 +18,7 @@ def cut_te(upper, lower):
     if u2.x > l2.x:
         interp_var = (l1.x - u2.x) / (l1.x - l2.x)
         lower[-1].x = upper[-2].x
-        lower[-1].y = interp_var * l2.y + (1-interp_var) * l1.y
+        lower[-1].y = interp_var * l2.y + (1 - interp_var) * l1.y
         upper[-1] = lower[-1]
     else:
         interp_var = (u1.x - l2.x) / (u1.x - u2.x)
@@ -25,6 +26,13 @@ def cut_te(upper, lower):
         upper[-1].y = interp_var * u2.y + (1 - interp_var) * u1.y
         lower[-1] = upper[-1]
 
+
+def move_pt(line, seg_idx, pos):
+    delta = pos - line[seg_idx].start
+    line[seg_idx - 1].end += delta
+    line[seg_idx - 1].control2 += delta
+    line[seg_idx].start += delta
+    line[seg_idx].control1 += delta
 
 
 # get bsplines traced from my very specific SVG. not portable, not generalised, terrible coding practice
@@ -43,12 +51,50 @@ lines = {"nacelle": [],
          "tail_zero_rad": [],
          "nosecone": []}
 
-Dfan = 68.3*unit('in').to_base_units()
-scale = abs((Dfan/2/(paths[7][0].poly()(0).imag-paths[6][0].poly()(0).imag)).magnitude)
-print(f"{scale = :f}")
+y0 = paths[6][0].start.imag
+# turbomachinery performance
+
+PW1100G = Engine(D=(81 * unit('in')).to('m'), BPR=12.5,
+                 sfc_cruise=0.0144 * unit('kg/kN/s'),
+                 eta_c=0.85, eta_f=0.92345, eta_t=0.9,
+                 r_pf=1.2, r_po=37, # 42
+                 Vjb_Vjc=0.8)
+
+LEAP1A = Engine(D=(78 * unit('in')).to('m'), BPR=11,
+                sfc_cruise=0.0144 * unit('kg/kN/s'),
+                eta_c=0.85, eta_f=0.92345, eta_t=0.9,
+                r_pf=1.22, r_po=35, # 40
+                Vjb_Vjc=0.8)
+
+condition = FlightCondition(M=0.78, L=3.8 * unit('m'), h=37000 * unit('ft'), units='SI')
+jet = JetCondition(condition, LEAP1A)
+# Ab_des = 2.449
+# Ac_des = 0.446
+Ab_des = jet.Ab.to_base_units()
+Ac_des = jet.Ac.to_base_units()
+
+scale = abs((jet.engine.D / 2 / (paths[7][0].start.imag - paths[6][0].start.imag)).magnitude)
+print(f"{jet.engine.D = :f}\n"
+      f"{scale = :f}")
+
+# outer radius stays same
+outer = scale * paths[1][0].end
+inner = scale * paths[2][1].start
+l0 = abs(outer - inner) * unit('m')
+# inner control point follows straight line from inital point to outside te
+rb_outer = abs(outer.imag - y0 * scale) * unit('m')
+delta_rb_l = abs(outer.imag - inner.imag) / l0 * unit('m')
+
+# A = np.pi*(rb_outer+l0*rb_inner_l)*l0
+# by quadratic formula
+# l = (-np.pi * rb_outer + np.sqrt(np.pi ** 2 * rb_outer ** 2 + 4 * rb_outer * Ab_des)) / 2 / rb_inner_l
+l = (rb_outer - np.sqrt(rb_outer ** 2 - delta_rb_l * Ab_des / np.pi)) / delta_rb_l
+t = l / l0
+print(f"{t = :f}")
+move_pt(paths[2], 1, t * inner / scale + (1 - t) * outer / scale)
 
 # interpolate through splines, 10 samples per spline segment
-t = np.linspace(0, 1, 10)
+t = np.linspace(0, 1, 1000)
 colors = [c for c in TABLEAU_COLORS.values()] + ['black']
 for i, key in enumerate(lines.keys()):
     for j, segment in enumerate(paths[i]):
@@ -75,22 +121,19 @@ lines["core_tail_outer"][0] = lines["core_iface"][-1]
 lines["core_tail_inner"][0] = lines["core_iface"][0]
 lines["core_tail_inner"][-1] = lines["tail_zero_rad"][0]
 
-y0 = lines["centreline"][-1].y.magnitude
-Ab = np.pi*((lines["bypass_iface"][0].y.magnitude-y0)**2-(lines["bypass_iface"][-1].y.magnitude-y0)**2)*unit('m^2')
-Ac = np.pi*((lines["core_iface"][-1].y.magnitude-y0)**2-(lines["core_iface"][0].y.magnitude-y0)**2)*unit('m^2')
-# POWER OF 10
-print(f"{Ab = }, {Ac = }")
 y0 = lines["centreline"][0].y.magnitude
-Ab = np.pi * ((lines["bypass_iface"][0].y.magnitude-y0)**2-(lines["bypass_iface"][-1].y.magnitude-y0)**2)
-Ac = np.pi * ((lines["core_iface"][-1].y.magnitude-y0)**2-(lines["core_iface"][0].y.magnitude-y0)**2)
+Ab = np.pi * ((lines["bypass_iface"][0].y.magnitude - y0) ** 2 - (lines["bypass_iface"][-1].y.magnitude - y0) ** 2)
+Ac = np.pi * ((lines["core_iface"][-1].y.magnitude - y0) ** 2 - (lines["core_iface"][0].y.magnitude - y0) ** 2)
 print(f"{Ab = :f}\n{Ac = :f}")
-exit()
 
 for i, key in enumerate(lines.keys()):
-    plt.plot([point.x.magnitude for point in lines[key]], [point.y.magnitude for point in lines[key]], '-', label=key, color=colors[i])
-plt.legend(loc="upper right")
-plt.axis("equal")
-plt.ylim(1.6,2.4)
+    plt.plot([point.x.magnitude for point in lines[key]], [y0 - point.y.magnitude for point in lines[key]], '-', label=key,
+             color='black')
+    # color=colors[i])
+# plt.legend(loc="upper right")
+
+plt.ylim(-1.5, 2.8)
+plt.gca().set_aspect('equal', adjustable='box')
 plt.show()
 exit()
 # draw lines in sketch segment by segment
