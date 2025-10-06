7ec51f58620b50c564b3fea8e739d229a7a9c18d
ec765 <ec765@cam.ac.uk>
2025-10-06 16:54:56 +0100

    fixed spacing with temperature and f2 looks correct. f1 not correct, reading Young 1992
    Signed-off-by: ec765 <ec765@cam.ac.uk>

--- mesh_shear.py
+++ mesh_shear.py
@@ -1,10 +1,13 @@
+import json
 import time
 from pathlib import Path
 
 from ansys.meshing import prime
 
+from setup import BoundaryCondition
+
 fname = Path("geom/shear.scdoc")
-t0=time.time()
+t0 = time.time()
 
 with (prime.launch_prime(n_procs=16, timeout=60) as prime_client):
     model = prime_client.model
@@ -28,8 +31,7 @@ with (prime.launch_prime(n_procs=16, timeout=60) as prime_client):
     print("Part summary:", summary)
 
     # global size control
-    model.set_global_sizing_params(prime.GlobalSizingParams(model, min=10.0, max=500, growth_rate=1+1/500))
-
+    model.set_global_sizing_params(prime.GlobalSizingParams(model, min=10.0, max=500, growth_rate=1.02))
 
     # periodic_control = model.control_data.create_periodic_control()
     # periodic_control.set_params(prime.PeriodicControlParams(model))
@@ -38,10 +40,11 @@ with (prime.launch_prime(n_procs=16, timeout=60) as prime_client):
     # print(f"created periodic control {periodic_control.id}")
 
     # curvature size controls
-    splitter_control = model.control_data.create_size_control(prime.SizingType.HARD)
+    splitter_control = model.control_data.create_size_control(prime.SizingType.CURVATURE)
     splitter_control.set_scope(prime.ScopeDefinition(model, evaluation_type=prime.ScopeEvaluationType.LABELS,
-                                                        label_expression="splitter*"))
-    splitter_control.set_hard_sizing_params(prime.HardSizingParams(model, min=10.0))
+                                                     label_expression="splitter*"))
+    splitter_control.set_curvature_sizing_params(
+        prime.CurvatureSizingParams(model, min=10.0, max=200, growth_rate=1.01, normal_angle=4, use_cad_curvature=True))
     print(f"created splitter size control {splitter_control.id} at {time.time() - t0:.2f} seconds")
 
     # wake_control = model.control_data.create_size_control(prime.SizingType.SOFT)
@@ -58,7 +61,7 @@ with (prime.launch_prime(n_procs=16, timeout=60) as prime_client):
 
     # compute size field
     size_field = prime.SizeField(model)
-    res=size_field.compute_volumetric(
+    res = size_field.compute_volumetric(
         [control.id for control in model.control_data.size_controls],
         prime.VolumetricSizeFieldComputeParams(model, enable_multi_threading=True))
     print(f"computed size field:\n{res}")
@@ -68,7 +71,7 @@ with (prime.launch_prime(n_procs=16, timeout=60) as prime_client):
     # mesh walls
     wall_params = prime.SurferParams(model=model, generate_quads=False, size_field_type=prime.SizeFieldType.VOLUMETRIC,
                                      enable_multi_threading=True)
-    wall_faces = part.get_topo_faces_of_label_name_pattern(f"*",
+    wall_faces = part.get_topo_faces_of_label_name_pattern(f"!*internal",
                                                            prime.NamePatternParams(model))
     print(surfer.mesh_topo_faces(part.id, wall_faces, wall_params))
     print(f"meshed {len(wall_faces)} wall faces: {wall_faces}")
@@ -89,28 +92,45 @@ with (prime.launch_prime(n_procs=16, timeout=60) as prime_client):
         prime.ScopeDefinition(model, evaluation_type=prime.ScopeEvaluationType.ZONES, zone_expression=f"freestream*"))
     freestream_control.set_params(prime.VolumeControlParams(model, prime.CellZoneletType.FLUID, skip_hexcore=True))
 
-    automesh_params = prime.AutoMeshParams(
-            model,
-            size_field_type=prime.SizeFieldType.VOLUMETRIC,
-            volume_fill_type=prime.VolumeFillType.TET,
-            prism_control_ids=[p.id for p in model.control_data.prism_controls],
-            volume_control_ids=[p.id for p in model.control_data.volume_controls],
-            periodic_control_ids=[p.id for p in model.control_data.periodic_controls],
-            multi_zone_control_ids=[p.id for p in model.control_data.multi_zone_controls()],
-            prism=prime.PrismParams(model, stair_step=prime.PrismStairStep(model, check_proximity=False)),
-            hexcore=prime.HexCoreParams(model,
-                                        transition_size_field_type=prime.SizeFieldType.VOLUMETRIC,
-                                        buffer_layers=2,
-                                        transition_layer_type=prime.HexCoreTransitionLayerType.DELETELARGE,
-                                        cell_element_type=prime.HexCoreCellElementType.ALLHEX,
-                                        enable_region_based_hexcore=True)
+    # # add BLs
+    # with open("geom/nacelle.json", "r") as f:
+    #     bcs = BoundaryCondition(**json.load(f))
+    # volume_scope = prime.ScopeDefinition(model, entity_type=prime.ScopeEntity.VOLUME)
+    #
+    # nacelle_bl = model.control_data.create_prism_control()
+    # nacelle_bl.set_surface_scope(
+    #     prime.ScopeDefinition(model, evaluation_type=prime.ScopeEvaluationType.ZONES, zone_expression="*_wall"))
+    # nacelle_bl.set_volume_scope(volume_scope)
+    # nacelle_bl.set_growth_params(prime.PrismControlGrowthParams(model,
+    #                                                             prime.PrismControlOffsetType.UNIFORM,
+    #                                                             first_height=bcs.bl_nacelle_bypass.y0 * 1000,
+    #                                                             growth_rate=bcs.bl_nacelle_bypass.GR,
+    #                                                             n_layers=bcs.bl_nacelle_bypass.n))
 
-        )
+    automesh_params = prime.AutoMeshParams(
+        model,
+        size_field_type=prime.SizeFieldType.VOLUMETRIC,
+        volume_fill_type=prime.VolumeFillType.TET,
+        prism_control_ids=[p.id for p in model.control_data.prism_controls],
+        volume_control_ids=[p.id for p in model.control_data.volume_controls],
+        periodic_control_ids=[p.id for p in model.control_data.periodic_controls],
+        multi_zone_control_ids=[p.id for p in model.control_data.multi_zone_controls()],
+        prism=prime.PrismParams(model, stair_step=prime.PrismStairStep(model, check_proximity=False)),
+        hexcore=prime.HexCoreParams(model,
+                                    transition_size_field_type=prime.SizeFieldType.VOLUMETRIC,
+                                    buffer_layers=2,
+                                    transition_layer_type=prime.HexCoreTransitionLayerType.DELETELARGE,
+                                    cell_element_type=prime.HexCoreCellElementType.ALLHEX,
+                                    enable_region_based_hexcore=True)
+
+    )
 
     print(f"began volume meshing at {time.time() - t0:.2f} seconds")
-    prime.AutoMesh(model).mesh(part_id=model.parts[0].id, automesh_params=automesh_params)
+    print(prime.AutoMesh(model).mesh(part_id=model.parts[0].id, automesh_params=automesh_params))
     print(f"completed volume meshing at {time.time() - t0:.2f} seconds")
 
     prime.FileIO(model).export_fluent_meshing_mesh(str(fname.with_suffix('.msh')),
-                                                       prime.ExportFluentMeshingMeshParams(model))
+                                                   prime.ExportFluentMeshingMeshParams(model))
     print(f"exported {str(fname.with_suffix('.msh'))} at {time.time() - t0:.2f} seconds")
+
+    print(model.parts[0])
